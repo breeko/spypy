9b5ab9fd060f5b24533b8a70a286cf6625e34326
Branko Blagojevic <branko.blagojevic@gmail.com>
2018-05-13 20:44:07 -0400

    Moved configs to new file. Bug fixes on timing detection

---
+++ model_configs.py
@@ -0,0 +1,7 @@
+ANCHORS_PATH = "model/yolov2.anchors"
+CLASSES_PATH = "model/yolov2.classes"
+CONFIG_PATH = "model/yolov2.cfg"
+WEIGHTS_PATH = "model/yolov2.weights" 
+MODEL_IMAGE_SIZE = (608, 608)
+MIN_CONFIDENCE = 0.5
+MAX_OVERLAP = 0.35
